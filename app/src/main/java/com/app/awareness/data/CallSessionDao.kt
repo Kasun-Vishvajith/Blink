dd38f13062e078be27a84f5cb9661423938a8f51
Kasun Vishvajith <kasunvishvajithuniversity@gmail.com>
2026-05-15 18:04:44 +0530

    Level 20.0

--- app/src/main/java/com/app/awareness/data/CallSessionDao.kt
+++ app/src/main/java/com/app/awareness/data/CallSessionDao.kt
@@ -14,4 +14,10 @@ interface CallSessionDao {
 
     @Query("SELECT * FROM call_sessions WHERE date = :date")
     fun queryByDate(date: String): Flow<List<CallSessionEntity>>
+
+    @Query("SELECT * FROM call_sessions WHERE package_name=:pkg AND date=:date AND end_time IS NULL LIMIT 1")
+    suspend fun getOpenSession(pkg: String, date: String): CallSessionEntity?
+
+    @Query("UPDATE call_sessions SET end_time=:endTime, duration_minutes=:mins WHERE id=:id")
+    suspend fun closeSession(id: Long, endTime: Long, mins: Int)
 }
